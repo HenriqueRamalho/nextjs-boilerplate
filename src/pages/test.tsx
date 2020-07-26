7d52823670119b5c43fb37dbe367cc53432274ed
Henrique Ramalho <henrique.ramalho@vamosparcelar.com.br>
2020-07-26 18:17:31 -0300

    configuracao e escrita de alguns components

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-
-import IndexPage from '.'
-
-describe('index page', () => {
-  it('should render the heading', () => {
-    render(<IndexPage />)
-  })
-})
